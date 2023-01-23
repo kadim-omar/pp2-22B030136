556ebf03a8ec9811ed5da99596b05ab3e2377c10
Kadim Omar <omar_01.04.2003@mail.ru>
2023-01-24 00:40:39 +0600

    6th commit

---
+++ multiple_values.py
@@ -0,0 +1,15 @@
+x, y, z="pillow", "grass", "car"
+print(x)
+print(y)
+print(z)
+
+x, y, z="pillow"
+print(x)
+print(y)
+print(z)
+
+cars = ["toyota", "bugatti", "dodge"]
+x, y, z = cars
+print(x)
+print(y)
+print(z)
