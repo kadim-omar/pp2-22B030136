9f8c4999a3d4a64f8518c0f4734c662924c51a82
Kadim Omar <omar_01.04.2003@mail.ru>
2023-01-24 00:09:53 +0600

    4th commit

---
+++ variables.py
@@ -0,0 +1,25 @@
+x=5
+y="Omar"
+print(x)
+print(y)
+
+x=4 #x is integer
+x="Omar01" #x is now string
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x=5 
+y="Omar01"
+print(type(x))
+print(type(y))
+
+y="Omar"
+#is the same as
+y='Omar'
+
+a=4
+A="570tenge"
+#a and A are considered different
